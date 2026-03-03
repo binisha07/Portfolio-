c0ee6296396b140e5f502a0050eb46fd311050ed
binisha07 <neupanebinisha082@gmail.com>
2026-03-03 16:16:30 +0545

    added footer section

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,21 @@
-const form = document.getElementById("contactForm");
-const nameInput = document.getElementById("name");
-const emailInput = document.getElementById("email");
-const messageInput = document.getElementById("message");
-const formMessage = document.getElementById("formMessage");
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("contactForm");
+    const nameInput = document.getElementById("name");
+    const emailInput = document.getElementById("email");
+    const messageInput = document.getElementById("message");
+    const formMessage = document.getElementById("formMessage");
 
-form.addEventListener("submit", function(e) {
-    e.preventDefault(); 
+    form.addEventListener("submit", function(e){
+        e.preventDefault();
 
-    if (nameInput.value === "" || 
-        emailInput.value === "" || 
-        messageInput.value === ""){
+        if (nameInput.value === "" || emailInput.value === "" || messageInput.value === ""){
+            formMessage.textContent = "Please fill all fields!";
+            formMessage.style.color = "red";
+        } else {
+            formMessage.textContent = "Message sent successfully!";
+            formMessage.style.color = "green";
 
-        formMessage.textContent = "Please fill all fields!";
-        formMessage.style.color = "red";
-
-    } else {
-        formMessage.textContent = "Message sent successfully!";
-        formMessage.style.color = "green";
-
-        form.reset();
-    }
+            form.reset();
+        }
+    });
 });
