03669543ae689e8933283fc08b9035bd02a725d4
binisha07 <neupanebinisha082@gmail.com>
2026-03-03 13:58:43 +0545

    added contact section

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+const form = document.getElementById("contactForm");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const messageInput = document.getElementById("message");
+const formMessage = document.getElementById("formMessage");
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault(); 
+
+    if (nameInput.value === "" || 
+        emailInput.value === "" || 
+        messageInput.value === ""){
+
+        formMessage.textContent = "Please fill all fields!";
+        formMessage.style.color = "red";
+
+    } else {
+        formMessage.textContent = "Message sent successfully!";
+        formMessage.style.color = "green";
+
+        form.reset();
+    }
+});
